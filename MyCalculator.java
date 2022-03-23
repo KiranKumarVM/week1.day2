8c4a8f038638b98245cdbf30c2669b6642e5ea9d
KiranKumarVM <97420501+KiranKumarVM@users.noreply.github.com>
2022-03-23 08:44:56 +0530

    Add files via upload

---
+++ MyCalculator.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class MyCalculator {
+
+	public static void main(String[] args) {
+		Calculator cal=new Calculator();
+		System.out.println("Addition " +cal.add(2, 3, 9));
+		System.out.println("Subtraction " +cal.sub(45, 3));
+		System.out.println("Multiplication "+cal.mul(23, 3));
+		System.out.println("division "+cal.div(24, 2));
+		
+		
+		
+
+	}
+
+}
